f4ddda6e27ed020395dcb48eecab5c5cdb67241f
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-12-23 15:51:02 +0600

    Add solution on task5

---
+++ Task5/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Напишите программу, 
+которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+int size = 4;
+int[,] numbers = new int[size, size];
+int value = 1;
+int i = 0;
+int j = 0;
+while (value <= numbers.Length)
+{
+    numbers[i, j] = value++;
+    if (i <= j + 1 && i + j < size - 1)
+        j++;
+    else if (i < j && i + j >= size - 1)
+        i++;
+    else if (i >= j && i + j > size - 1)
+        j--;
+    else
+        i--;
+}
+WriteMatrix(numbers);
+
+void WriteMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] <= 9)
+                Console.Write(array[i, j] + "  ");
+            else
+                Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
